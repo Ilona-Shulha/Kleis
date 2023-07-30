b361146112ee442ae855d1455a4a82f5c79230ff
Ilona Shulha <ilona.sh.ilo@gmail.com>
2023-07-30 15:12:33 +0300

    dist init

---
+++ assets/Contacts-6ba72a8d.js
@@ -0,0 +1,1 @@
+import{_ as e,n as t,y as _}from"./index-ebb23a10.js";const c={};function n(a,o){return t(),_("div",null," Contacts ")}const r=e(c,[["render",n],["__scopeId","data-v-74eda010"]]);export{r as default};
